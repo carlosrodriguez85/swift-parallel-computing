ead6e0236fd3090c119a3f37848cb9ff264d8d72
Carlos Rodriguez Dominguez <carlos@everywaretech.es>
2016-06-25 22:07:38 +0200

    Initial Commit

---
+++ ParallelComputing/ParallelComputing.h
@@ -0,0 +1,19 @@
+//
+//  ParallelComputing.h
+//  ParallelComputing
+//
+//  Created by Carlos Rodríguez Domínguez on 25/6/16.
+//  Copyright © 2016 Everyware Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ParallelComputing.
+FOUNDATION_EXPORT double ParallelComputingVersionNumber;
+
+//! Project version string for ParallelComputing.
+FOUNDATION_EXPORT const unsigned char ParallelComputingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ParallelComputing/PublicHeader.h>
+
+
