cbc0d589e90b50eb7d0f76943dbea973e2af1ca0
Carlos Rodriguez Dominguez <carlos@everywaretech.es>
2016-06-26 02:26:41 +0200

    Import of UIKit framework removed.

--- ParallelComputing/ParallelComputing.h
+++ ParallelComputing/ParallelComputing.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Everyware Technologies. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for ParallelComputing.
 FOUNDATION_EXPORT double ParallelComputingVersionNumber;
